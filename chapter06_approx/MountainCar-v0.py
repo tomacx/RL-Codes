dcc97077ae9d2c9e2cefc8bdcb6a8b7e4c4942fb
tomacx <139841470+tomacx@users.noreply.github.com>
2025-11-03 11:28:45 +0800

    函数近似+DQN算法

--- chapter06_approx/MountainCar-v0.py
+++ chapter06_approx/MountainCar-v0.py
@@ -216,7 +216,7 @@ c = ax.pcolormesh(positions, velocities, v_values, shading='auto')
 ax.set_xlabel('position')
 ax.set_ylabel('velocity')
 fig.colorbar(c, ax=ax);
-plt.show()
+# plt.show()
 
 # 绘制动作估计
 
@@ -232,4 +232,128 @@ c = ax.pcolormesh(positions, velocities, action_values, shading='auto')
 ax.set_xlabel('position')
 ax.set_ylabel('velocity')
 fig.colorbar(c, ax=ax, boundaries=[-.5, .5, 1.5, 2.5], ticks=[0, 1, 2]);
-plt.show()
+# plt.show()
+
+# 深度Q网络求解最优策略
+print("深度Q网络")
+
+class Chart:
+    def __init__(self):
+        self.fig, self.ax = plt.subplots(1, 1)
+        # plt.ion()
+
+    def plot(self, episode_rewards):
+        self.ax.clear()
+        self.ax.plot(episode_rewards)
+        self.ax.set_xlabel('iteration')
+        self.ax.set_ylabel('episode reward')
+        self.fig.canvas.draw()
+
+# 经验回放 补充：代码和原理
+class DQNReplayer:
+    def __init__(self, capacity):
+        self.memory = pd.DataFrame(index=range(capacity),
+                                   columns=['observation', 'action', 'reward', 'next_observation', 'terminated'])
+        self.i = 0
+        self.count = 0
+        self.capacity = capacity
+
+    def store(self, *args):
+        self.memory.loc[self.i] = np.asarray(args, dtype=object)
+        self.i = (self.i + 1) % self.capacity
+        self.count = min(self.count + 1, self.capacity)
+
+    def sample(self, size):
+        indices = np.random.choice(self.count, size)
+        return (np.stack(self.memory.loc[indices, field]) for field in
+                self.memory.columns)
+
+# DQN
+class DQNAgent:
+    def __init__(self, env, net_kwargs={}, gamma=0.99, epsilon=0.001,
+                 replayer_capacity=10000, batch_size=64):
+        observation_dim = env.observation_space.shape[0]
+        self.action_n = env.action_space.n
+        self.gamma = gamma
+        self.epsilon = epsilon
+
+        self.batch_size = batch_size
+        self.replayer = DQNReplayer(replayer_capacity) # 经验回放
+
+        self.evalute_net = self.build_network(input_size=observation_dim,
+                                              output_size=self.action_n, **net_kwargs) # 评估网络
+        self.target_net = self.build_network(input_size=observation_dim,
+                                             output_size=self.action_n, **net_kwargs) # 目标网络
+        self.target_net.set_weights(self.evalute_net.get_weights())
+
+    def build_network(self, input_size, hidden_sizes, output_size,
+                    activation=tf.nn.relu, output_activation=None,
+                    learning_rate=0.01): # 构建网络
+        model = keras.Sequential()
+        for layer, hidden_size in  enumerate(hidden_sizes):
+            kwargs = dict(input_shape=(input_size,)) if not layer else {}
+            model.add(keras.layers.Dense(units=hidden_size,
+                                         activation=activation, **kwargs))
+        model.add(keras.layers.Dense(units=output_size,
+                                     activation=output_activation)) # 输出层
+        optimizer = tf.optimizers.Adam(learning_rate)
+        model.compile(loss='mse', optimizer=optimizer)
+        return model
+
+    def learn(self, observation, action, reward, next_observation, terminated, truncated):
+        self.replayer.store(observation, action, reward, next_observation,
+                            terminated) # 存储经验
+        observations, actions, rewards, next_observations, terminateds = \
+                                    self.replayer.sample(self.batch_size) # 经验回放
+
+        next_qs = self.target_net.predict(next_observations, verbose=0)
+        next_max_qs = next_qs.max(axis=-1)
+        us = rewards + self.gamma * (1. - terminateds) * next_max_qs
+        targets = self.evalute_net.predict(observations, verbose=0)
+        targets[np.arange(us.shape[0]), actions] = us
+        self.evalute_net.fit(observations, targets, verbose=0)
+
+        if terminated or truncated: # 更新目标网络
+            self.target_net.set_weights(self.evalute_net.get_weights())
+
+    def decide(self, observation): # epsilon贪心策略
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(self.action_n)
+        qs = self.evalute_net.predict(observation[np.newaxis], verbose=0)
+        return np.argmax(qs)
+
+def play_qlearning(env, agent, train=False, render=False):
+    episode_reward = 0
+    observation, _ = env.reset()
+    while True:
+        if render:
+            env.render()
+        action = agent.decide(observation)
+        next_observation, reward, terminated, truncated, _ = env.step(action)
+        episode_reward += reward
+        if train:
+            agent.learn(observation, action, reward, next_observation,
+                    terminated, truncated)
+        if terminated or truncated:
+            break
+        observation = next_observation
+    return episode_reward
+
+
+net_kwargs = {'hidden_sizes' : [64, 64], 'learning_rate' : 0.001}
+agent = DQNAgent(env, net_kwargs=net_kwargs)
+
+# 训练
+episodes = 500
+episode_rewards = []
+chart = Chart()
+for episode in range(episodes):
+    episode_reward = play_qlearning(env, agent, train=True)
+    episode_rewards.append(episode_reward)
+    chart.plot(episode_rewards)
+
+# 测试
+agent.epsilon = 0. # 取消探索
+episode_rewards = [play_qlearning(env, agent) for _ in range(100)]
+print('平均回合奖励 = {} / {} = {}'.format(sum(episode_rewards),
+        len(episode_rewards), np.mean(episode_rewards)))
